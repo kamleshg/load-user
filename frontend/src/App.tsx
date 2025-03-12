dd7c0f0dfdd1d112517ec35a361b564e19a60a21
Kamlesh Gokal <gokal@kamleshs-mbp.lan>
2025-03-12 19:11:45 -0400

    Add update/delete

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,14 +4,23 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import CreateUser from './components/createUser.tsx';
 import ReadUsers from './components/readUsers.tsx';
+import UpdateUsers from './components/updateUser.tsx';
+import DeleteUsers from './components/deleteUser.tsx';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+        <div>
+          <a href="https://docs.squid.cloud" target="_blank">
+          <img src={reactLogo} className="logo" alt="Squid Cloud logo" />
+        </a>
+      </div>
       <CreateUser />
       <ReadUsers />
+      <UpdateUsers />
+      <DeleteUsers />
     </>
   )
 }
